2b57a4ab6d4dad040029d6b2b124a4c74586b9aa
narjeswehbe <narjeswehbi04@gmail.com>
2023-01-05 08:53:54 +0200

    added CustomUsersDetailsService to manage login and register

--- src/main/java/com/narjes/jwt/services/CustomUsersDetailsService.java
+++ src/main/java/com/narjes/jwt/services/CustomUsersDetailsService.java
@@ -1,23 +1,69 @@
 package com.narjes.jwt.services;
 
-import org.springframework.security.core.userdetails.User;
+import com.narjes.jwt.entity.Role;
+import com.narjes.jwt.entity.User;
+import com.narjes.jwt.model.RoleModel;
+import com.narjes.jwt.model.UserModel;
+import com.narjes.jwt.repository.RoleRepository;
+import com.narjes.jwt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
 @Service
+@RequiredArgsConstructor
 public class CustomUsersDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private  BCryptPasswordEncoder passwordEncoder=new BCryptPasswordEncoder();
     //validates the user
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if(username.equals("Narjes"))
-        {
-            return new User("Narjes" , "secret" , new ArrayList<>());
-        }else {
-            throw new UsernameNotFoundException("user name not found");
-        }
+
+         User user = userRepository.findUserByUsername(username);
+         if(user==null) throw new  UsernameNotFoundException("user not found!");
+        UserModel userModel = new UserModel();
+        BeanUtils.copyProperties(user, userModel);
+        return userModel;
 
     }
+    public UserModel register(UserModel userModel){
+
+        User userEntity = new User();
+        BeanUtils.copyProperties(userModel, userEntity);//it does not do a deep copy
+
+        Set<Role> roleEntities = new HashSet<>();
+        //fetch every role from DB based on role id and than set this role to user entity roles
+        for(RoleModel rm :userModel.getRoles()){
+            Optional<Role> optRole = roleRepository.findById(rm.getId());
+            optRole.ifPresent(roleEntities::add);
+        }
+        userEntity.setRoles(roleEntities);
+        userEntity.setPassword(this.passwordEncoder.encode(userModel.getPassword()));
+        userEntity = userRepository.save(userEntity);
+
+        BeanUtils.copyProperties(userEntity, userModel);
+
+        //convert RoleEntities to RoleModels
+        Set<RoleModel> roleModels = new HashSet<>();
+        RoleModel rm = null;
+        for(Role re :userEntity.getRoles()){
+            rm = new RoleModel();
+            rm.setRoleName(re.getRoleName());
+            roleModels.add(rm);
+        }
+        userModel.setRoles(roleModels);
+
+
+        return userModel;
+    }
+
 }
