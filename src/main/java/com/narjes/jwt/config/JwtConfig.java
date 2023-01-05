c695b6902431ddaa64e5268b2ecbd228f3c0f4ce
narjeswehbe <narjeswehbi04@gmail.com>
2023-01-05 08:52:49 +0200

    added jwt configuration

--- src/main/java/com/narjes/jwt/config/JwtConfig.java
+++ src/main/java/com/narjes/jwt/config/JwtConfig.java
@@ -1,5 +1,6 @@
 package com.narjes.jwt.config;
 
+import com.narjes.jwt.filter.JwtAuthenticationFilter;
 import com.narjes.jwt.services.CustomUsersDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -10,36 +11,44 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class JwtConfig extends WebSecurityConfigurerAdapter {
     private final CustomUsersDetailsService customUsersDetailsService;
+    private final JwtAuthenticationFilter jwtFilter;
+    private final jwtAuthEntryPoint jwtAuthEntryPoint;
     //control which end points are permitted
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.headers().frameOptions().disable();
         http
                 .csrf()
                 .disable()
                 .cors()
                 .disable()
                 .authorizeRequests()
-                .antMatchers("/api/login").permitAll()
-                .anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .antMatchers("/api/login"   , "/api/register" , "/api/unrestricted").permitAll()
+                .anyRequest().authenticated().and()
+                .exceptionHandling().authenticationEntryPoint(jwtAuthEntryPoint)
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         ;
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
    // how the authentication is managed?
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(customUsersDetailsService);
+        auth.userDetailsService(customUsersDetailsService).passwordEncoder(passwordEncoder());
     }
     @Bean
-    public PasswordEncoder passwordEncoder()
-    {
-    return NoOpPasswordEncoder.getInstance();
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
